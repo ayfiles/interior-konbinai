1f7727085af3b884f928f4a3dbd87ab8e67b71d5
TheFiles.io <thefiles.io@gmail.com>
2025-11-24 13:26:42 +0100

    chore: route contact emails to konbinai visual

--- supabase/functions/send-notification/index.ts
+++ supabase/functions/send-notification/index.ts
@@ -84,7 +84,7 @@ const handler = async (req: Request): Promise<Response> => {
 
     const emailResponse = await resend.emails.send({
       from: "Konbinai Notifications <onboarding@resend.dev>",
-      to: ["aytac.files@gmail.com"],
+      to: ["konbinai.visual@gmail.com"],
       subject: subject,
       html: emailHtml,
     });
